4ff04fbf39a9f45e3a07c0bc418689adf59ecbcb
Your Name <martinkobersky@gmail.com>
2020-01-18 12:27:04 +0100

    init

---
+++ src/main/scala/cz/copr/chess/Main.scala
@@ -0,0 +1,9 @@
+package cz.copr.chess
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    ChessServer.stream[IO].compile.drain.as(ExitCode.Success)
+}<
