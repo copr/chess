62eb78bf81f6fd0690609074219761e1ebca09a9
Martin Koběrský <martinkobersky@gmail.com>
2020-05-01 21:58:27 +0200

    remove jokes routes
    Jokes are part of initial template used

--- src/main/scala/cz/copr/chess/server/ChessServer.scala
+++ src/main/scala/cz/copr/chess/server/ChessServer.scala
@@ -13,27 +13,28 @@ import scala.concurrent.ExecutionContext.global
 object ChessServer {
 
   def stream[F[_]: ConcurrentEffect](implicit T: Timer[F], C: ContextShift[F]): Stream[F, Nothing] = {
-    for {
-      client <- BlazeClientBuilder[F](global).stream
-      helloWorldAlg = HelloWorld.impl[F]
-      jokeAlg = Jokes.impl[F](client)
-
-      // Combine Service Routes into an HttpApp.
-      // Can also be done via a Router if you
-      // want to extract a segments not checked
-      // in the underlying routes.
-      httpApp = (
-        ChessRoutes.helloWorldRoutes[F](helloWorldAlg) <+>
-        ChessRoutes.jokeRoutes[F](jokeAlg)
-      ).orNotFound
-
-      // With Middlewares in place
-      finalHttpApp = Logger.httpApp(true, true)(httpApp)
-
-      exitCode <- BlazeServerBuilder[F]
-        .bindHttp(8080, "0.0.0.0")
-        .withHttpApp(finalHttpApp)
-        .serve
-    } yield exitCode
-  }.drain
+    ???
+  }
+//    for {
+//      client <- BlazeClientBuilder[F](global).stream
+//      helloWorldAlg = HelloWorld.impl[F]
+//      jokeAlg = Jokes.impl[F](client)
+//
+//      // Combine Service Routes into an HttpApp.
+//      // Can also be done via a Router if you
+//      // want to extract a segments not checked
+//      // in the underlying routes.
+//      httpApp = (
+//        ChessRoutes.chessRoutes[F](helloWorldAlg) <+>
+//      ).orNotFound
+//
+//      // With Middlewares in place
+//      finalHttpApp = Logger.httpApp(true, true)(httpApp)
+//
+//      exitCode <- BlazeServerBuilder[F]
+//        .bindHttp(8080, "0.0.0.0")
+//        .withHttpApp(finalHttpApp)
+//        .serve
+//    } yield ()
+//  }.drain
 }
