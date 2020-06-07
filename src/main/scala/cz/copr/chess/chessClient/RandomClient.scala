df8b4b7199021b721f69811ed082f56d2ab9a1f7
Martin Koběrský <martinkobersky@gmail.com>
2020-06-07 19:35:44 +0200

    add random client

---
+++ src/main/scala/cz/copr/chess/chessClient/RandomClient.scala
@@ -0,0 +1,19 @@
+package cz.copr.chess.chessClient
+
+import cats.implicits._
+import cats.effect.Sync
+import cz.copr.chess.chessLogic.{ ChessLogic, ChessState, Move }
+
+class RandomClient[F[_] : Sync](val random: scala.util.Random) extends ChessClient[F] {
+
+  def getMove(gameState: ChessState): F[Move] = {
+    val moves = ChessLogic.getAllPossibleMoves(gameState.switchTeam).toVector
+    for {
+      index <- Sync[F].delay(random.nextInt(moves.length))
+      move   = moves(index)
+      _     <- Sync[F].delay(println(moves))
+    } yield move
+  }
+
+  def inform(msg: String): F[Unit] = Sync[F].delay(println(msg))
+}
--- src/main/scala/cz/copr/chess/main.scala
+++ src/main/scala/cz/copr/chess/main.scala
@@ -1,11 +1,12 @@
 package cz.copr.chess
 
-import cats.implicits._
-import cats.mtl.implicits._
+import scala.util.Random
+
 import cats.data.StateT
 import cats.effect.{ ExitCode, IO, IOApp, SyncConsole }
-import cz.copr.chess.chessClient.ConsoleClient
-import cz.copr.chess.chessLogic.{ ChessLogic, ChessState }
+import cats.mtl.implicits._
+import cz.copr.chess.chessClient.{ ConsoleClient, RandomClient }
+import cz.copr.chess.chessLogic.ChessState
 import cz.copr.chess.game.Game
 import cz.copr.chess.gameClient.LocalClient
 
@@ -19,12 +20,11 @@ object main extends IOApp {
 
     val initGame = ChessState.createInitialState
 
-    val player1 = ConsoleClient[LocalMonad]
-    val player2 = ConsoleClient[LocalMonad]
-
-    val game = new Game[LocalMonad](player1, player2, LocalClient[LocalMonad])
-
     for {
+      random <- IO.delay(new Random)
+      player1 = ConsoleClient[LocalMonad]
+      player2 = new RandomClient[LocalMonad](random)
+      game    = new Game[LocalMonad](player1, player2, LocalClient[LocalMonad])
       result <- game.playUntilTheEnd.run(initGame)
       _      <- IO(println(result._1.gameResult))
     } yield ExitCode.Success
